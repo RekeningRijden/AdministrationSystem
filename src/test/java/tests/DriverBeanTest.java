bc58f37baa54276fa2833e72f15d185b1b9f755b
Hoeks, Maikel <maikelhoeks@hotmail.com>
2016-04-12 18:43:30 +0200

    testen weer aangezet

--- src/test/java/tests/DriverBeanTest.java
+++ src/test/java/tests/DriverBeanTest.java
@@ -1,40 +1,40 @@
-//package tests;
-//
-//import org.jboss.arquillian.container.test.api.Deployment;
-//import org.jboss.arquillian.junit.Arquillian;
-//import org.jboss.shrinkwrap.api.Archive;
-//import org.junit.After;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//
-//import javax.inject.Inject;
-//
-//import main.domain.Driver;
-//import main.service.DriverService;
-//
-///**
-// * @author Sam
-// */
-//@RunWith(Arquillian.class)
-//public class DriverBeanTest {
-//
-//    @Deployment
-//    public static Archive<?> createDeployment() {
-//        return TestHelper.createDeployment();
-//    }
-//
-//    @Inject
-//    private DriverService driverService;
-//
-//    @After
-//    public void after() {
-//        for (Driver driver : driverService.getAll()) {
-//            driverService.remove(driver);
-//        }
-//    }
-//
-//    @Test
-//    public void placeHolderTest() {
-//
-//    }
-//}
+package tests;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import main.domain.Driver;
+import main.service.DriverService;
+
+/**
+ * @author Sam
+ */
+@RunWith(Arquillian.class)
+public class DriverBeanTest {
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        return TestHelper.createDeployment();
+    }
+
+    @Inject
+    private DriverService driverService;
+
+    @After
+    public void after() {
+        for (Driver driver : driverService.getAll()) {
+            driverService.remove(driver);
+        }
+    }
+
+    @Test
+    public void placeHolderTest() {
+
+    }
+}
